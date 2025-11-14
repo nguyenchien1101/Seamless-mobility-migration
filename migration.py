ee5fe4030067b8ea2fb8a0ce52671b622892055e
nguyenchien1101 <vonguyenchien1101@gmail.com>
2025-11-14 22:14:34 +0700

    fix_controller_ver3

--- migration.py
+++ migration.py
@@ -1,90 +1,159 @@
 from mininet.net import Mininet
-from mininet.node import RemoteController
+from mininet.node import Controller, RemoteController, OVSKernelSwitch, Host
 from mininet.cli import CLI
-from mininet.log import setLogLevel
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink
 
 class CustomCLI(CLI):
-    def do_movesw(self, _line):
-        """Interrupt s1-s2 and connect s1-s4"""
-        print(">>> Interrupt s1-s2 and connect s1-s4")
+
+    def do_move_h(self, line):
+        args = line.split()
+        if len(args) != 2:
+            print("Usage: move_h <host> <dest_switch>")
+            return
+
+        host_name, dest_sw_name = args
         net = self.mn
 
-        s1 = net.get('s1')
-        s4 = net.get('s4')
+        try:
+            host = net.get(host_name)
+            dest_sw = net.get(dest_sw_name)
+        except KeyError:
+            print("Error: host or switch does not exist!")
+            return
 
-        # Interrupt s1-s2
-        net.configLinkStatus('s1', 's2', 'down')
+        print(f"HOST MIGRATION: moving {host_name} → {dest_sw_name}")
 
-        # Create new link s1-s4
-        link = net.addLink(s1, s4)
-        link.intf1.ifconfig('up')
-        link.intf2.ifconfig('up')
+        # 1) Find old switch automatically
+        old_link = None
+        for link in net.links:
+            if link.intf1.node == host or link.intf2.node == host:
+                old_link = link
+                break
 
-        s1.attach(link.intf1.name)
-        s4.attach(link.intf2.name)
-        print(">>> Completed: s1 connected to s4")
+        if not old_link:
+            print(f"ERROR: No link found between {host_name} and any switch.")
+            return
 
-    def do_moveho(self, _line):
-        print(">>> Migrating h2 from s1 to s2")
+        # Identify old switch
+        old_sw = old_link.intf1.node if old_link.intf1.node != host else old_link.intf2.node
+        old_sw_name = old_sw.name
+
+        print(f" - Found old connection: {host_name} ←→ {old_sw_name}")
+
+        # Bring down old link
+        net.configLinkStatus(old_sw_name, host_name, "down")
+
+        # 2) Create new link
+        print(f" - Creating new link: {host_name} ←→ {dest_sw_name}")
+        newlink = net.addLink(dest_sw, host)
+
+        newlink.intf1.ifconfig('up')
+        newlink.intf2.ifconfig('up')
+
+        # Re-apply same IP
+        ip = host.IP()
+        host.setIP(ip, intf=newlink.intf2)
+
+        dest_sw.attach(newlink.intf1.name)
+
+        print(f">>> Migration complete: {host_name} is now connected to {dest_sw_name}")
+
+    # ------------------------------------------------------------------------
+
+    def do_move_sw(self, line):
+        args = line.split()
+        if len(args) != 3:
+            print("Usage: move_sw <switch1> <switch2> <switch3>")
+            return
+
+        sw1_name, sw2_name, sw3_name = args
         net = self.mn
 
-        s1 = net.get('s1')
-        s2 = net.get('s2')
-        h2 = net.get('h2')
+        try:
+            sw1 = net.get(sw1_name)
+            sw2 = net.get(sw2_name)
+            sw3 = net.get(sw3_name)
+        except KeyError:
+            print("Error: switch does not exist!")
+            return
 
-        # Interrupt s1-h2
-        net.configLinkStatus('s1', 'h2', 'down')
+        print(f">>> Interrupt {sw1_name}-{sw2_name} and connect {sw1_name}-{sw3_name}")
 
-        # Create new link s2-h2
-        link = net.addLink(s2, h2)
-        link.intf1.ifconfig('up') 
-        link.intf2.ifconfig('up')
+        # Remove old link properly
+        old_links = net.linksBetween(sw1, sw2)
+        if old_links:
+           for link in old_links:
+              net.delLink(link)
+        else:
+           print(" - WARNING: no existing link to remove")
 
-        h2.setIP('10.0.0.2/8', intf=link.intf2)
-        s2.attach(link.intf1.name)
+        # Create new link
+        print(f" - Creating new link {sw1_name}-{sw3_name}")
+        newlink = net.addLink(sw1, sw3)
 
+        newlink.intf1.ifconfig('up')
+        newlink.intf2.ifconfig('up')
 
-def create_sdn_network():
-    setLogLevel('info')
+        sw1.attach(newlink.intf1.name)
+        sw3.attach(newlink.intf2.name)
 
-    net = Mininet()
+        print(f">>> SWITCH MIGRATION completed: {sw1_name} now connected to {sw3_name}")
 
-    print("Adding controller")
-    c0 = net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6633)
+        
+        
+# ----------------------
+# Network topology
+# ----------------------
+def myNetwork():
 
-    print("Adding hosts")
-    h1 = net.addHost('h1')
-    h2 = net.addHost('h2')
-    h3 = net.addHost('h3')
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
 
-    print("Adding switches")
-    s1 = net.addSwitch('s1')
-    s2 = net.addSwitch('s2')
-    s3 = net.addSwitch('s3')
-    s4 = net.addSwitch('s4')
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=RemoteController,
+                      protocol='tcp',
+                      port=6633)
 
-    print("Creating links")
-    net.addLink(s1, h1)
+    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(s4, s3)
+    net.addLink(h1, s1)
     net.addLink(s1, h2)
     net.addLink(s3, h3)
     net.addLink(s1, s2)
     net.addLink(s2, s3)
-    net.addLink(s3, s4)
 
-    print("Starting network")
+    info( '*** Starting network\n')
     net.build()
-    c0.start()
-    s1.start([c0])
-    s2.start([c0])
-    s3.start([c0])
-    s4.start([c0])
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
 
-    net.pingFull()
+    info( '*** Starting switches\n')
+    net.get('s1').start([c0])
+    net.get('s2').start([c0])
+    net.get('s3').start([c0])
+    net.get('s4').start([c0])
 
-    print("Running CLI")
+    info('*** Ready — starting Custom CLI\n')
     CustomCLI(net)
-    print("Stopping network")
+
     net.stop()
 
 if __name__ == '__main__':
-    create_sdn_network()
+    setLogLevel('info')
+    myNetwork()
+    
--- mobility.py
+++ mobility.py
@@ -1,36 +1,51 @@
 #!/usr/bin/env python
 import os
+import sys
+import time
+
 from mininet.log import setLogLevel, info
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.cli import CLI
-import matplotlib.pyplot as plt
 from mininet.node import RemoteController
-import sys
 from mininet.term import makeTerm
-import time
+
+import matplotlib.pyplot as plt
+
+
+def trigger_link_failure(cli, ap='ap2', iface='ap2-wlan1', down_time=4):
+
+    info(f"\n*** [Trigger] LINK FAILURE on {ap}:{iface}\n")
+
+    cmd_down = f"{ap} ifconfig {iface} down"
+    print(cli.prompt + cmd_down)
+    cli.onecmd(cmd_down)
+
+    info(f"*** {iface} is DOWN for {down_time} seconds...\n")
+    time.sleep(down_time)
+
+    cmd_up = f"{ap} ifconfig {iface} up"
+    print(cli.prompt + cmd_up)
+    cli.onecmd(cmd_up)
+
+    info(f"*** {iface} is UP again, forcing STA re-association...\n")
+    # Ép các STA roaming join lại SSID ssid-1
+    cli.onecmd('sta1 iwconfig sta1-wlan0 essid "ssid-1"')
+    cli.onecmd('sta2 iwconfig sta2-wlan0 essid "ssid-1"')
+
+    info("*** sta1 & sta2 reassociated to ssid-1\n")
+
 
 class BatchCLI(CLI):
-    def __init__(self, net, **kwargs):
-        super().__init__(net)
+
+    def do_lf(self, line):
+        trigger_link_failure(self, ap='ap2', iface='ap2-wlan1', down_time=4)
+
+    def help_lf(self):
+        info("lf: trigger link failure on ap2-wlan1 (down ~4s rồi up lại)\n")
 
     def run(self):
-        info("*** Port ap1-wlan1 is going to be inactive for the next 10 seconds!!!\n")
-        time.sleep(10.0)
-        command = 'ap1 ifconfig ap1-wlan1 down'
-        print(self.prompt + command)
-        self.onecmd(command)
-
-        info("*** Port ap1-wlan1 is going to be active again for the next 7 seconds!!!\n")
-        time.sleep(7.0)
-        command = 'ap1 ifconfig ap1-wlan1 up'
-        print(self.prompt + command)
-        self.onecmd(command)
-        
-        time.sleep(3.0)
-        self.onecmd('sta1 iwconfig sta1-wlan0 essid "ssid-1"')
-        self.onecmd('sta2 iwconfig sta2-wlan0 essid "ssid-1"')
-
         return super().run()
+
 
 def topology(args):
     net = Mininet_wifi()
@@ -38,57 +53,82 @@ def topology(args):
     info("*** Creating nodes\n")
     h1 = net.addHost('h1', ip='10.0.0.1/8')
 
-    sta1 = net.addStation('sta1', ip='10.0.0.3/8', position='150,70,0')  # Near ap2
-    sta2 = net.addStation('sta2', ip='10.0.0.2/8', position='50,80,0')   # Near ap1
+    # STA:
+    # - sta1 & sta2: roaming giữa ap1 và ap2
+    # - sta3: đứng cố định gần ap3 (client tĩnh)
+    sta2 = net.addStation('sta2', ip='10.0.0.2/8', position='60,70,0')    # gần ap1 lúc đầu
+    sta1 = net.addStation('sta1', ip='10.0.0.3/8', position='140,50,0')   # gần ap2 lúc đầu
+    sta3 = net.addStation('sta3', ip='10.0.0.4/8', position='100,120,0')  # client tĩnh của ap3
 
-    ap1 = net.addAccessPoint('ap1', ssid='ssid-1', mode='g', channel='1', position='50,50,0', range=40)
-    ap2 = net.addAccessPoint('ap2', ssid='ssid-1', mode='g', channel='1', position='150,80,0', range=40)
+    # APs sắp thành tam giác cho đẹp:
+    #  - ap1: góc trái dưới
+    #  - ap2: góc phải dưới
+    #  - ap3: trên giữa
+    ap1 = net.addAccessPoint(
+        'ap1', ssid='ssid-1', mode='g',
+        channel='1', position='40,40,0', range=45
+    )
+    ap2 = net.addAccessPoint(
+        'ap2', ssid='ssid-1', mode='g',
+        channel='1', position='160,40,0', range=45
+    )
+    ap3 = net.addAccessPoint(
+        'ap3', ssid='ssid-1', mode='g',
+        channel='1', position='100,130,0', range=45
+    )
 
     s1 = net.addSwitch('s1')
-    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
+    c0 = net.addController(
+        'c0', controller=RemoteController,
+        ip='127.0.0.1', port=6633
+    )
 
-    info("*** Configuring environment\n")
+    info("*** Configuring WiFi environment\n")
     net.setPropagationModel(model="logDistance", exp=4.5)
     net.configureNodes()
-    #net.set ('ssf')
-    info("*** Creating links\n")
+
+    info("*** Creating wired links (ap1/ap2/ap3/h1 <-> s1)\n")
     net.addLink(ap1, s1)
     net.addLink(ap2, s1)
+    net.addLink(ap3, s1)
     net.addLink(h1, s1)
 
-    info("*** Show plot\n")
+    info("*** Showing Mininet-WiFi graph (triangle topology)\n")
     net.plotGraph(max_x=200, max_y=150)
-
-    info("*** Starting mobility\n")
-    net.startMobility(time=0)
-
-    # Mobility for sta2: go near ap2
-    net.mobility(sta2, 'start', time=1, position='50,80,0')
-    net.mobility(sta2, 'stop', time=7, position='130,80,0')
-
-    # Mobility for sta1: go near ap1
-    net.mobility(sta1, 'start', time=1, position='150,70,0')
-    net.mobility(sta1, 'stop', time=7, position='70,60,0')
-
-    net.stopMobility(time=3600)
     plt.ion()
     plt.show(block=False)
 
-    info("*** Starting network\n")
+
+    net.startMobility(time=0)
+
+    net.mobility(sta2, 'start', time=8,  position='60,70,0')
+    net.mobility(sta2, 'stop',  time=13, position='150,70,0')
+
+    net.mobility(sta1, 'start', time=8,  position='140,50,0')
+    net.mobility(sta1, 'stop',  time=13, position='50,50,0')
+
+    net.stopMobility(time=20)
+
+    info("*** Starting network & controller\n")
     net.build()
     c0.start()
     ap1.start([c0])
     ap2.start([c0])
+    ap3.start([c0])
     s1.start([c0])
+
+    info("*** Installing static ARP entries\n")
     net.staticArp()
+
+    time.sleep(3)
+
     makeTerm(
         h1,
         title='h1-ping-sta2',
         term='xterm',
-        cmd="bash -c 'ping 10.0.0.2; exec bash'"
+        cmd="bash -c 'ping -i 0.3 -D 10.0.0.2; exec bash'"
     )
 
-    info("*** CLI\n")
     BatchCLI(net)
 
     info("*** Stopping network\n")
@@ -97,6 +137,8 @@ def topology(args):
     info("*** Closing all xterms\n")
     os.system('killall xterm')
 
+
 if __name__ == '__main__':
     setLogLevel('info')
     topology(sys.argv)
+
